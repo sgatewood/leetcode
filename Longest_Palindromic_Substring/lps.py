9014477ed788a2fc42b54da21d2c7d219e77bc34
Sean Gatewood <stg2bd@virginia.edu>
2019-10-11 02:47:05 -0400

    Pass leetcode MVP

--- Longest_Palindromic_Substring/lps.py
+++ Longest_Palindromic_Substring/lps.py
@@ -1,3 +1,43 @@
+# 2) Grow palindromes outward:
+# -----------------------------------
+def get_all_palindromes(s):
+    palindromes = []
+    for i in range(len(s)):
+        palindromes.append((i,i))
+        if i < len(s) - 1 and s[i] == s[i+1]:
+            palindromes.append((i,i+1))
 
-def lps(s):
-    return ""
+    while len(palindromes) != 0:
+        palindromes2 = []
+        for p in palindromes:
+            start = p[0]
+            end = p[1]
+            yield s[start:end+1]
+            if start == 0 or end == len(s)-1:
+                continue
+            if s[start-1] == s[end+1]:
+                palindromes2.append((start-1,end+1))
+
+        palindromes = palindromes2
+
+def grow_lps(s):
+    max_palindrome = ""
+    for p in get_all_palindromes(s):
+        if len(p) > len(max_palindrome):
+            max_palindrome = p
+    return max_palindrome
+
+# 1) Brute force
+# -----------------------------------
+
+def get_all_substrings(s
+    for i in range(len(s)):
+        for j in range(i+1,len(s)+1):
+            yield(s[i:j])
+
+def brute_force_lps(s):
+    max_found = ""
+    for substring in get_all_substrings(s):
+        if is_palindrome(substring) and len(substring) > len(max_found):
+            max_found = substring
+    return max_found
--- Longest_Palindromic_Substring/test.py
+++ Longest_Palindromic_Substring/test.py
@@ -1,36 +1,31 @@
 from lps import lps
+import unittest
 
-tests = {}
+class TestLPS(unittest.TestCase):
 
-def add_test(name,args,expected):
-    if type(args) != tuple:
-        args = (args,)
-    tests[name] = (args,expected)
+    def test_example_1(self):
+        actual = lps("babad")
+        self.assertTrue(actual in ("bab","aba"))
 
-def run_all_tests(f):
-    num_passed = 0
-    for name in tests:
-        args,expected = tests[name]
-        arg_string = "(%r)" % args[0] if len(args) == 1 else str(args)
-        print("\033[1;36m>>>",f.__name__ + arg_string ,"\033[0m")
-        try:
-            actual = f(*args)
-            print("Output:","%r" % actual)
-            if(expected != actual):
-                print("\033[1;33mExpected: %r" % expected ,"\033[0m")
-            else:
-                print("\033[1;32m✅ PASS" ,"\033[0m")
-                num_passed += 1
-        except Exception as e:
-            print("\033[1;31m" + type(e).__name__ + ":", e ,"\033[0m")
+    def test_example_2(self):
+        actual = lps("cbbd")
+        expected = "bb"
+        self.assertEqual(actual,expected)
 
-        print()
+    def test_palindrome(self):
+        actual = lps("racecar")
+        expected = "racecar"
+        self.assertEqual(actual,expected)
 
-    num_failed = len(tests) - num_passed
-    print("Passed: %d" % num_passed)
-    print("Failed: %d" % num_failed)
+    def test_empty(self):
+        actual = lps("")
+        expected = ""
+        self.assertEqual(actual,expected)
 
-add_test("Example 1",("babad"),expected = "aba")
-add_test("Example 2",("cbbd"),expected = "bb")
+    def test_one(self):
+        actual = lps("")
+        expected = ""
+        self.assertEqual(actual,expected)
 
-run_all_tests(lps)
+if __name__ == '__main__':
+    unittest.main()
