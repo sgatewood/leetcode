fd4ac739738c6384cf8e95932b9b2ab4f70203ce
Sean Gatewood <stg2bd@virginia.edu>
2019-10-11 03:36:00 -0400

    Yields

--- Longest_Palindromic_Substring/lps.py
+++ Longest_Palindromic_Substring/lps.py
@@ -1,26 +1,28 @@
+
+
 # 2) Grow palindromes outward:
 # -----------------------------------
-def get_all_palindromes(s):
-    palindromes = []
+
+def get_expansions(p,s):
+    start,end = p
+    yield s[start:end+1]
+    while (start > 0 and end < len(s)-1) and s[start-1] == s[end+1]:
+        start -= 1
+        end += 1
+        yield s[start:end+1]
+
+def get_initial_palindromes(s):
     for i in range(len(s)):
-        palindromes.append((i,i))
+        yield (i,i)
         if i < len(s) - 1 and s[i] == s[i+1]:
-            palindromes.append((i,i+1))
+            yield (i,i+1)
 
-    while len(palindromes) != 0:
-        palindromes2 = []
-        for p in palindromes:
-            start = p[0]
-            end = p[1]
-            yield s[start:end+1]
-            if start == 0 or end == len(s)-1:
-                continue
-            if s[start-1] == s[end+1]:
-                palindromes2.append((start-1,end+1))
+def get_all_palindromes(s):
+    for p in get_initial_palindromes(s):
+        for e in get_expansions(p,s):
+            yield e
 
-        palindromes = palindromes2
-
-def grow_lps(s):
+def lps(s):
     max_palindrome = ""
     for p in get_all_palindromes(s):
         if len(p) > len(max_palindrome):
@@ -30,7 +32,7 @@ def grow_lps(s):
 # 1) Brute force
 # -----------------------------------
 
-def get_all_substrings(s
+def get_all_substrings(s):
     for i in range(len(s)):
         for j in range(i+1,len(s)+1):
             yield(s[i:j])
